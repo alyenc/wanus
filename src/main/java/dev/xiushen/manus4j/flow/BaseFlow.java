874641edf132c1d7ded2bbb7451d6b722cf42bdb
alyenc <alyenc@outlook.com>
2025-03-26 22:18:57 +0800

    init

---
+++ src/main/java/dev/xiushen/manus4j/flow/BaseFlow.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2025 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package dev.xiushen.manus4j.flow;
+
+import dev.xiushen.manus4j.agent.BaseAgent;
+
+import java.util.Map;
+
+public abstract class BaseFlow {
+
+	protected Map<String, BaseAgent> agents;
+
+	public BaseFlow(Map<String, BaseAgent> agents, Map<String, Object> data) {
+		this.agents = agents;
+		data.put("agents", agents);
+	}
+
+	public abstract String execute(String inputText);
+
+}
---
+++ src/main/java/dev/xiushen/manus4j/package-info.java
@@ -0,0 +1,1 @@
+package dev.xiushen.manus4j;
