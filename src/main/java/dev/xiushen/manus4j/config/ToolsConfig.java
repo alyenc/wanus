7c6c507e27d4a8620e2de8879c9443bcf64adf91
alyenc <alyenc@outlook.com>
2025-03-29 23:36:07 +0800

    把第三方MCP注入到agent

--- src/main/java/dev/xiushen/manus4j/config/ToolsConfig.java
+++ src/main/java/dev/xiushen/manus4j/config/ToolsConfig.java
@@ -3,16 +3,23 @@ package dev.xiushen.manus4j.config;
 import dev.xiushen.manus4j.common.ChromeDriver;
 import dev.xiushen.manus4j.tool.*;
 import jakarta.annotation.Resource;
+import org.springframework.ai.model.function.FunctionCallback;
 import org.springframework.ai.tool.ToolCallbackProvider;
 import org.springframework.ai.tool.method.MethodToolCallbackProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Configuration
 public class ToolsConfig {
 
 	@Resource
 	private ChromeDriver chromeDriver;
+	@Resource
+	private ToolCallbackProvider toolCallbackProvider;
 
 	@Bean
 	public ToolCallbackProvider planningToolCallbackProvider() {
@@ -21,9 +28,12 @@ public class ToolsConfig {
 				.build();
 	}
 
+	// TODO现在将外部的MCP服务全部注入到了同一个Provider，后续看是否拆开
 	@Bean
 	public ToolCallbackProvider manusToolCallbackProvider() {
-		return MethodToolCallbackProvider.builder()
+		toolCallbackProvider.getToolCallbacks();
+
+		List<FunctionCallback> toolCallbacks = Arrays.stream(MethodToolCallbackProvider.builder()
 				.toolObjects(
 //						new WebSearchService(),
 						new LocalTimeService(),
@@ -32,6 +42,10 @@ public class ToolsConfig {
 						new PythonService(),
 						new DocLoaderService()
 				)
-				.build();
+				.build()
+				.getToolCallbacks()).collect(Collectors.toList());
+
+		toolCallbacks.addAll(Arrays.stream(toolCallbackProvider.getToolCallbacks()).toList());
+		return ToolCallbackProvider.from(toolCallbacks);
 	}
 }
